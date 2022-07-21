8944eef4cffc154a70639377ded24cfa96b10dc2
Daniel <daniel@artcity.pl>
2022-07-21 10:20:22 +0200

    Update DancingText.jsx

--- DancingText.jsx
+++ DancingText.jsx
@@ -20,7 +20,7 @@ if(sel.length) {
         var frame = sel[a];
         if(frame.constructor.name == 'TextFrame') {
             for (var i = 0; i < frame.parentStory.characters.length; i += 2) {
-                frame.parentStory.characters[i].baselineShift = 1.5;
+                frame.parentStory.characters[i].baselineShift = myPointSize;
             }
         }
     }
